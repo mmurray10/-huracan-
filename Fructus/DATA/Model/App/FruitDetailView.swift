f88c90127b991d7b350df75875d5d537b99c9005
mmurray10 <majoria@me.com>
2023-06-15 03:00:03 -0700

    App Restart button added

--- Fructus/DATA/Model/App/FruitDetailView.swift
+++ Fructus/DATA/Model/App/FruitDetailView.swift
@@ -61,6 +61,7 @@ struct FruitDetailView: View {
             } //: SCROLL
             .edgesIgnoringSafeArea(.top)
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
